7a709142959bdde06732926fda601ce8daf8c03a
denisnica.242004@gmail.com <denisnica.242004@gmail.com>
2022-11-07 13:15:53 -0600

    despliegue de página de descargar

---
+++ main.js
@@ -0,0 +1,5 @@
+const buttonDownload = document.querySelector('.download__main button');
+
+buttonDownload.addEventListener('click', () => {
+  location.href = 'https://denisamador24.github.io/medi-serv/medi-serv.apk';
+});
